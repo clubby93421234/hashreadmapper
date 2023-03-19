63d37f03a261195e7f112d641aa0dad943276d73
pmurk <127439026+pmurk@users.noreply.github.com>
2023-03-19 14:07:33 +0100

    Update mappedread.cuh
    from Chat GPT

--- include/gpu/mappedread.cuh
+++ include/gpu/mappedread.cuh
@@ -1,3 +1,11 @@
+/*Dieser Code definiert die Struktur "MappedRead", die zur Speicherung von Informationen zu einer gelesenen DNA-Sequenz verwendet wird.
+Die Struktur enthält mehrere Mitgliedsvariablen, darunter "orientation", "hammingDistance", "shift", "chromosomeId" und "position", 
+die jeweils spezifische Informationen über die gelesene Sequenz enthalten. 
+Die "AlignmentOrientation" ist eine Enumeration, die den Orientierungstyp der Sequenz angibt.
+Der Rest der Variablen speichert Informationen über den Leseprozess, einschließlich der Anzahl der Hamming-Abstände,
+der Verschiebung, der Chromosomen-ID und der Position. 
+Die Header-Datei dient dazu, die Struktur zu definieren und sie anderen Teilen des Programms zugänglich zu machen.*/
+
 #ifndef MAPPEDREAD_CUH
 #define MAPPEDREAD_CUH
 
